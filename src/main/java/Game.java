9fd77f8d1ac8fc72fa7965b422f783399d701a52
Ezzerof <christian.bitca@gmail.com>
2022-08-16 19:43:36 +0100

    new initialization for board and excluding wrong inputs

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game extends Board {
@@ -10,17 +13,25 @@ public class Game extends Board {
         initSnake();
 
         while (isOn) {
-            System.out.println("Score: " + getScore() + " | " + "Snake length: " + getSnakeLen());
-            System.out.print("Choose direction with W,S,D,A or X to exit: ");
-            String direction = scanner.next().toLowerCase();
+            Character [] accChar = new Character[]{'a', 'd', 'w', 's'};
+            String direction = getUserInput(scanner);
+
 
             if (direction.equals("w") || direction.equals("s") || direction.equals("a") || direction.equals("d")) {
                 sneakMovement(direction);
             } else {
-                isOn = false;
+                printBoard();
+                System.out.println("Wrong letter, please use any of direction letters");
             }
 
         }
+    }
+
+    private String getUserInput(Scanner scanner) {
+        System.out.println("Score: " + getScore() + " | " + "Snake length: " + getSnakeLen());
+        System.out.print("Choose direction with W,S,D,A or X to exit: ");
+        String direction = scanner.next().toLowerCase();
+        return direction;
     }
 
 
